268074ed5c005e105246838192c7ee3d2de8a248
riantechy <korirbrianchr5@gmail.com>
2023-09-18 18:40:07 +0300

    Jikoconnect project

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -161,9 +161,13 @@ TAGGIT_CASE_INSENSITIVE = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
